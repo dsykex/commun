c63030f8ff5210a650e407cdb5d748381c0b4ffc
Chris Jones <WarholMac@Chriss-iMac.local>
2019-06-14 03:23:56 +0700

    Created basic authentication.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {AngularFireModule} from '@angular/fire';
 import {AngularFirestoreModule} from '@angular/fire/firestore';
+import { HttpClientModule } from '@angular/common/http';
 
 export const fbConfig = {
   apiKey: "AIzaSyDdeEahrBiiFWcghRiZRxZWYcTSPCgdYo8",
@@ -26,6 +27,8 @@ export const fbConfig = {
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
     AngularFireModule.initializeApp(fbConfig),
     AngularFirestoreModule,
     AppRoutingModule
